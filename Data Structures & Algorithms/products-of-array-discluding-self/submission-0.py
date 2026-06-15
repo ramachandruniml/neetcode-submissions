286d766074d2a9a152466292db35b13505464420
ramachandruniml <ramachandruniml@gmail.com>
2026-06-14 23:00:34 -0500

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        output=[]
+        size= len(nums)
+        total=1
+        for i in range(size):
+            newArray = nums
+            popped_Element =newArray.pop(i)
+            total = math.prod(newArray)
+            print(total)
+            output.insert(i, total)
+            newArray.insert(i, popped_Element)
+            
+        
+        return output
